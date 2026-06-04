1f0b8d8808ec0b82f27afa5bc63db56ee03826a9
moonman89 <79048472+moonman89@users.noreply.github.com>
2026-06-05 02:25:16 +0300

    Load product image treatment styling

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/index.css";
 import "./styles/luxury-overrides.css";
+import "./styles/product-image-treatment.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
